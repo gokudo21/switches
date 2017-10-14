0813baa65ae9274611a99104728c1db020b8f0a3
Ahmad Alobaid <ahmad88me@gmail.com>
2017-10-14 21:46:29 +0200

    fixing the web server

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
+
+#@app.route("/js/<path:path>")
+#def js(path):
+#    print "redirecting to :"+path
+#    return redirect("/static/js/"+path)
+
+
+@app.route("/<path:path>")
+def js(path):
+    print "redirecting to :"+path
+    return redirect("/static/"+path)
+
+
 @app.route("/")
 def hello():
     return render_template('index.html')
